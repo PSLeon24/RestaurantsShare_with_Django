8da43d97c5a77b74673b266c9db55378adb9508f
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-12-30 04:45:28 +0900

    implement categoryCreate

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, "shareRes/index.html")
+    categories = Category.objects.all()
+    content = {"categories": categories}
+    return render(request, "shareRes/index.html", content)
 
 
 def restaurantDetail(request):
@@ -16,4 +20,20 @@ def restaurantCreate(request):
 
 
 def categoryCreate(request):
-    return render(request, "shareRes/categoryCreate.html")
+    categories = Category.objects.all()
+    content = {"categories": categories}
+    return render(request, "shareRes/categoryCreate.html", content)
+
+
+def Create_category(request):
+    category_name = request.POST["categoryName"]
+    new_category = Category(category_name=category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse("cateCreatePage"))
+
+
+def Delete_category(request):
+    category_id = request.POST["categoryId"]
+    delete_category = Category.objects.get(id=category_id)
+    delete_category.delete()
+    return HttpResponseRedirect(reverse("cateCreatePage"))
